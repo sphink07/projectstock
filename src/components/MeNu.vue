8419b7d7bf6a2478ea4d492aa3b2fc4c1c6f3e52
sphink07 <fromgun4455@gmail.com>
2022-08-11 12:01:28 +0700

    fix1

---
+++ src/components/MeNu.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+      <v-navigation-drawer 
+        dark
+        permanent
+        height="650"
+      >
+        <v-divider></v-divider>
+        <v-list 
+          nav
+          dense
+        >
+            <v-list-item link>
+                <v-list-item-icon>
+                    <v-icon color="#FFFF66">mdi-clipboard-text</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>รายการ</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+                <v-list-item-icon>
+                    <v-icon color="#66FF66">mdi-briefcase-plus-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>เบิกของ</v-list-item-title>
+            </v-list-item>            
+            <v-list-item link>
+                <v-list-item-icon>
+                    <v-icon color="#00CCFF">mdi-clipboard-text-clock-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>รายการที่เบิกไปแล้ว</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+                <v-list-item-icon>
+                    <v-icon color="#DDDDDD">mdi-cog-transfer-outline</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>จัดการคลัง</v-list-item-title>
+                </v-list-item>
+
+        </v-list>
+      </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+
+export default {
+   //
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,20 @@
 <template>
 <div class="home">
-<h1>dsifsdhjodsijg</h1>
+  <v-row no-gutters>
+    <v-col cols="2">
+      <MeNu></MeNu>
+    </v-col>
+      <v-col cols="10">
+      <v-card height="650">sdfsdf</v-card>
+    </v-col>
+  </v-row>  
 </div>
 </template>
 
 <script>
-
+import MeNu from '@/components/MeNu.vue';
   export default {
     name: 'HoMe',
+    components : { MeNu }
   }
 </script>
