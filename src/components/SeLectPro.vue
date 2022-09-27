b38d8159dd5aec928be956088107df47f05fa50e
sphink07 <fromgun4455@gmail.com>
2022-09-27 11:39:06 +0700

    h6j76534tre

--- src/components/SeLectPro.vue
+++ src/components/SeLectPro.vue
@@ -13,23 +13,40 @@
             <v-row no-gutters>
               <v-col class="mt-6">
                 <v-row>
-                    <span class=" hidden-sm-and-down mt-2" 
-                    style="margin-top:10px ; font-size:22px ; margin-right:35px ; margin-left:20px ; color:#ffffff ; "
-                      > ✅ เลือกรายการที่ต้องการเบิก 
-                    </span>
-                    <v-text-field
+                  <span
+                    class="hidden-sm-and-down mt-2"
+                    style="
+                      margin-top: 10px;
+                      font-size: 22px;
+                      margin-right: 35px;
+                      margin-left: 20px;
+                      color: #ffffff;
+                    "
+                  >
+                    ✅ เลือกรายการที่ต้องการเบิก
+                  </span>
+                  <v-text-field
                     class="ml-5 mr-5"
                     v-model="search"
                     append-icon="mdi-magnify"
                     label="Search"
                     solo
                   ></v-text-field>
-                  
-                  <v-btn class="mr-10 hidden-sm-and-down" href="/select/list" height="48" 
-                  style=" border-radius:10px; margin-left:40px ; background-color:#505050 ; color:#ffffff">
+
+                  <v-btn
+                    @click="ChackZero()"
+                    class="mr-10 hidden-sm-and-down"
+                    height="48"
+                    style="
+                      border-radius: 10px;
+                      margin-left: 40px;
+                      background-color: #505050;
+                      color: #ffffff;
+                    "
+                  >
                     <v-badge color="#43a839" inline style>
-                      <template v-slot:badge >
-                        <div>{{Length}}</div>
+                      <template v-slot:badge>
+                        <div>{{ Length }}</div>
                       </template>
                       รายการที่เลือก
                     </v-badge>
@@ -48,17 +65,28 @@
             <v-row no-gutters>
               <v-col class="mt-2 text-center">
                 <v-text-field
-                v-model="search"
-                append-icon="mdi-magnify"
-                label="Search"
-                solo
-              ></v-text-field>
-                <v-btn block elevation="10" style="margin-top:-15px; background-color:#505050 ; color:#ffffff ;" href="/select/list" width="120">
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Search"
+                  solo
+                ></v-text-field>
+                <v-btn
+                  @click="ChackZero()"
+                  block
+                  elevation="10"
+                  style="
+                    margin-top: -15px;
+                    background-color: #505050;
+                    color: #ffffff;
+                  "
+                  width="120"
+                >
                   <v-badge color="#43a839" inline style>
                     <template v-slot:badge>
-                      <div>{{Length}}</div>
+                      <div>{{ Length }}</div>
                     </template>
-                    รายการที่เลือก </v-badge>
+                    รายการที่เลือก
+                  </v-badge>
                 </v-btn>
               </v-col>
             </v-row>
@@ -66,77 +94,98 @@
         </v-col>
       </v-row>
       <v-card height="86vh">
-        <v-data-table 
-        height="77vh"
-        flat
-        :headers="headers"
-        :items="datafilter"
-        :search="search"
-        :footer-props="footerProps"
-        show-select
-        v-model="dataselect"
-      ></v-data-table>
+        <v-data-table
+          height="77vh"
+          flat
+          :headers="headers"
+          :items="datafilter"
+          :search="search"
+          :footer-props="footerProps"
+          show-select
+          v-model="dataselect"
+        ></v-data-table>
       </v-card>
     </v-app>
   </div>
 </template>
 
-<script> 
-
-export default { 
+<script>
+export default {
   data() {
     return {
-       Length:0,
-       datafilter:[],
-       dataselect:[],
-       datafilter2:[],
-       search:'',
-       footerProps: {'items-per-page-options': [11,20,25]},
-       headers: [
-          {
-            text: 'Name',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Codenumber', value: 'code' },
-          { text: 'Quantity', value: 'qty' },
-        ],
-
-    }
+      booleen: true,
+      Length: 0,
+      datafilter: [],
+      dataselect: [],
+      datafilter2: [],
+      search: "",
+      footerProps: { "items-per-page-options": [11, 20, 25] },
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Codenumber", value: "code" },
+        { text: "Quantity", value: "qty" },
+      ],
+    };
   },
   methods: {
-  //      Testdata(a) {
-  //        let data1 = a
-  //       for(let i = 0 ; i < data1.length ; i++ ){
-  //        this.datafilter[i] = data1[i].name
-  //       }
-  //       this.datafilter2 = this.datafilter
-  //       console.log(this.datafilter2)
-  //      },
-  //     Search(){
-  //       const resu = this.datafilter2.filter((x) => x == this.dataname)
-  //       console.log(resu)
-  //  },
-
+    //      Testdata(a) {
+    //        let data1 = a
+    //       for(let i = 0 ; i < data1.length ; i++ ){
+    //        this.datafilter[i] = data1[i].name
+    //       }
+    //       this.datafilter2 = this.datafilter
+    //       console.log(this.datafilter2)
+    //      },
+    //     Search(){
+    //       const resu = this.datafilter2.filter((x) => x == this.dataname)
+    //       console.log(resu)
+    //  },
+    ChackZero() {
+      for (let i = 0; i < this.dataselect.length; i++) {
+        if (this.dataselect[i].qty <= 0) {
+          this.booleen = false;
+        } else {
+          this.booleen = true;
+        }
+      }
+      if (this.dataselect.length == 0) {
+        this.booleen = true;
+      }
+      if (this.booleen === true) {
+        this.$router.push("/select/list");
+      } else {
+        alert("รายการที่เลือกมีรายการที่สินค้าที่หมด ไม่สามารถดำเนินการต่อได้");
+      }
+    },
   },
-mounted() {
-      this.axios.get("http://localhost:3000/stock").then((response) => {
+  mounted() {
+    this.axios.get("http://localhost:3000/stock").then((response) => {
       this.datafilter = response.data;
-    }); 
-},
-updated() {
-  this.Length = this.dataselect.length
-},
+    });
+  },
+  updated() {
+    this.Length = this.dataselect.length;
+    let JsonString = JSON.stringify(this.dataselect);
+    localStorage.setItem("Iteminlist", JsonString);
+  },
 };
 </script>
 
 <style scoped>
-  .wh{
-    width: 100vw;
-  }
-  .col00{
-    background: rgb(54,54,54);
-    background: linear-gradient(90deg, rgba(54,54,54,1) 0%, rgba(173,173,173,1) 100%);
-  }
+.wh {
+  width: 100vw;
+}
+.col00 {
+  background: rgb(54, 54, 54);
+  background: linear-gradient(
+    90deg,
+    rgba(54, 54, 54, 1) 0%,
+    rgba(173, 173, 173, 1) 100%
+  );
+}
 </style>
--- src/views/SelectList.vue
+++ src/views/SelectList.vue
@@ -10,21 +10,35 @@
             offset-sm="1"
             style="text-align: center"
           >
-            <v-card height="600" elevation="5"> 
-              <v-data-table 
-              height="77vh"
-              flat
-              :headers="headers"
-              :items="datafilter"
-              :search="search"
-              :footer-props="footerProps"
-              hide-default-footer
-            ></v-data-table>
+            <v-card height="600" elevation="5">
+              <v-data-table
+                height="85vh"
+                flat
+                :headers="headers"
+                :items="ListShow"
+                hide-default-footer
+                ><template v-slot:[`item.action`]="{ item }">
+                  <td style="width: 180px">
+                    <v-text-field
+                      v-model="textinput[item.id]"
+                      :rules="rules"
+                      class="mt-7"
+                      solo
+                      type="number"
+                      single-line
+                      label="0"
+                      min="1"
+                      outlined
+                    >
+                    </v-text-field>
+                  </td>
+                </template>
+              </v-data-table>
             </v-card>
             <v-col>
               <v-container>
                 <v-btn
-                  href="/select"
+                  @click="backpage()"
                   class="my-8 mr-8 hidden-sm-and-down"
                   height="40"
                   width="250"
@@ -32,12 +46,17 @@
                 >
                   back
                 </v-btn>
-                <v-btn class="my-8 cbtn hidden-sm-and-down" height="40" width="250">
+                <v-btn
+                  @click="TestSlot()"
+                  class="my-8 cbtn hidden-sm-and-down"
+                  height="40"
+                  width="250"
+                >
                   submit
                 </v-btn>
                 <!-- ________________________________sm and down_____________________________________ -->
                 <v-btn
-                  href="/select"
+                  @click="backpage()"
                   class="hidden-md-and-up ma-2"
                   height="40"
                   width="250"
@@ -45,7 +64,12 @@
                 >
                   back
                 </v-btn>
-                <v-btn class=" cbtn hidden-md-and-up ma-2" height="40" width="250">
+                <v-btn
+                  @click="TestSlot()"
+                  class="cbtn hidden-md-and-up ma-2"
+                  height="40"
+                  width="250"
+                >
                   submit
                 </v-btn>
                 <!-- ________________________________________________________________________________ -->
@@ -59,8 +83,52 @@
 </template>
 
 <script>
+import Swal from "sweetalert2";
 export default {
+  data() {
+    return {
+      Mixlist: [],
+      textinput: [],
+      textinputfix: [],
+      ListShow: [],
+      headers: [
+        {
+          text: "Name",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        { text: "Codenumber", value: "code" },
+        { text: "Codenumber", value: "code" },
+        { text: "", value: "action" },
+      ],
+      rules: [(value) => !!value || "กรอกจำนวนให้ครบถ้วน."],
+    };
+  },
   components: {},
+  mounted() {
+    let JsonData = JSON.parse(localStorage.getItem("Iteminlist"));
+    this.ListShow = JsonData;
+    // console.log(this.ListShow);
+  },
+  methods: {
+    TestSlot() {
+      this.textinputfix = this.textinput.filter(function (e) {
+        return e;
+      });
+      if (this.textinputfix.length == this.ListShow.length) {
+        for (let i = 0; i < this.textinputfix.length; i++) {
+          this.Mixlist[i] = this.ListShow[i].qty - this.textinputfix[i];
+        }
+        console.log(this.Mixlist);
+      } else {
+
+      }
+    },
+    backpage() {
+      this.$router.push("/select");
+    },
+  },
 };
 </script>
 
@@ -75,8 +143,11 @@ export default {
   );
 }
 .cbm {
-  background: rgb(54,54,54);
-  background: linear-gradient(90deg, rgba(54,54,54,1) 0%, rgba(173,173,173,1) 100%);
-
+  background: rgb(54, 54, 54);
+  background: linear-gradient(
+    90deg,
+    rgba(54, 54, 54, 1) 0%,
+    rgba(173, 173, 173, 1) 100%
+  );
 }
 </style>
